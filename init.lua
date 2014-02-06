92b53183fdb4e5f87ad7444d3e86430ec9c5d994
Alejandro Baez <Alejan.Baez@gmail.com>
2014-02-05 20:09:43 -0500

    Initial Commit

---
+++ init.lua
@@ -0,0 +1,73 @@
+textadept.file_types.extensions.rs = 'rust'
+
+
+-- Table of Rust-specific key bindings.
+-- @class table
+-- @name _G.keys.ansi_c
+keys.rust = {
+  [keys.LANGUAGE_MODULE_PREFIX] = {
+    m = {io.open_file, _HOME..'/modules/rust/init.lua'},
+  },
+  ['s\n'] = function()
+    buffer:line_end()
+    buffer:add_text(';')
+    buffer:new_line()
+  end,
+}
+
+if type(snippets) == 'table' then
+  snippets.rust = {
+
+    -- functions
+    fn    = "fn %1(name)(%2(param)) %3(-> %4(type) ){\n\t%0\n}",
+    gfn   = "fn %1(name)<%2(T)>(%3(%4(param): &[T])) %5(-> %6(&[T])) {\n\t%0\n}",
+    divfn = "fn %1(name)(%2(param)) ! {\n\t%0\n}",
+
+    -- Items and Attributes
+    extern  = "extern %1(\"ABI\") {\n\t%0\n}",
+    struct  = "struct %1(name) {\n\t%0\n}",
+    enum    = "enum %1(name) {\n\t%0\n}",
+    mod     = "mod %1(name) {\n\t%0\n}",
+    trait   = "trait %1(name) {\n\t%0\n}",
+    impl    = "impl %1(name) %2(for %3(type)) {\n\t%0\n}",
+
+    -- Expressions
+    v           = "[%1(start), ..%2(range_end)] %0",
+    l           = "|%1(param)| %0(expr)",
+    ["while"]   = "while %1(expr) {\n\t%0\n}",
+    loop        = "loop {\n\t%0\n}",
+    ["do"]      = "do |%1(param)| {\n\t%0\n}",
+    ["for"]     = "for %1(pattern) in %2(expr) {\n\t%0\n}",
+
+    -- if
+    ["if"]      = "if %1(expr) {\n\t%0\n}",
+    ["ife"]     = "if %1(expr) {\n\t%1(block)\n} else %0(expr)",
+    match       = "match %1(expr) {\n\t%2(pattern) => %0(expr)\n}",
+
+
+
+    -- random
+    ["#"]   = "#[%0(attribute)]",
+    static  = "static %1(name): %2(type) = %0;",
+    lmut    = "let mut %1(name): %2(type) = %0;",
+    ["/*"]  = "/*\n\t%0\n*/"
+  }
+end
+
+
+events.connect(events.LEXER_LOADED, function (lang)
+  if lang ~= 'rust' then return end
+
+  buffer.tab_width = 4
+  buffer.tab_indents = false
+
+  -- semantic highlighting
+  buffer.property['style.operator'] = 'fore:%(color.base0F)'
+  buffer.property['style.identifier'] = 'fore:%(color.base0D)'
+  buffer.property['style.function'] = 'fore:%(color.base08)'
+  buffer.property['style.constant'] = 'fore:%(color.base0E)'
+  buffer.property['style.library'] = 'fore:%(color.base09)'
+  buffer.property['style.keyword'] = 'fore:%(color.base04)'
+end)
+
+return {}
---
+++ rust.lua
@@ -0,0 +1,64 @@
+-- Copyright 2014 Alejandro Baez <Alejan.Baez@gmail.com> See LICENSE.
+-- Rust LPeg lexer.
+
+local l, token, word_match = lexer, lexer.token, lexer.word_match
+local P, R, S = lpeg.P, lpeg.R, lpeg.S
+
+local M = {_NAME = 'rust'}
+
+-- Whitespace.
+local ws = token(l.WHITESPACE, l.space^1)
+
+-- Comments.
+local line_comment = '//' * l.nonnewline_esc^0
+local block_comment = '/*' * (l.any - '*/')^0 * P('*/')^-1
+local comment = token(l.COMMENT, line_comment + block_comment)
+
+-- Strings.
+local sq_str = P('L')^-1 * l.delimited_range("'", true)
+local dq_str = P('L')^-1 * l.delimited_range('"', true)
+local string = token(l.STRING, sq_str + dq_str)
+
+-- Numbers.
+local number = token(l.NUMBER, l.float + l.integer)
+--local number = number .. token(l.NUMBER, '0b' * l.integer)
+
+-- Keywords.
+local keyword = token(l.KEYWORD, word_match{
+  'as', 'break', 'do', 'else', 'enum', 'extern', 'false', 'fn', 'for', 'if',
+  'impl', 'in', 'let', 'loop', 'match', 'mod', 'mut', 'priv', 'pub', 'ref',
+  'return', 'self', 'static', 'struct', 'super', 'true', 'trait', 'type',
+  'unsafe', 'use', 'while'
+})
+
+-- Types.
+local type = token(l.TYPE, word_match{
+  '()', 'bool', 'int', 'uint', 'char', 'str',
+  'u8', 'u16', 'u32', 'u64', 'i8', 'i16', 'i32', 'i64',
+  'binary32', 'binary64', 'f32','f64',
+})
+
+-- Identifiers.
+local identifier = token(l.IDENTIFIER, l.word)
+
+-- Operators.
+local operator = token(l.OPERATOR, S('+-/*%<>!=`^~@&|?#~:;,.()[]{}'))
+
+M._rules = {
+  {'whitespace', ws},
+  {'keyword', keyword},
+  {'type', type},
+  {'identifier', identifier},
+  {'string', string},
+  {'comment', comment},
+  {'number', number},
+  {'operator', operator},
+}
+
+M._foldsymbols = {
+  _patterns = {'%l+', '[{}]', '/%*', '%*/', '//'},
+  [l.OPERATOR] = {['{'] = 1, ['}'] = -1},
+  [l.COMMENT] = {['/*'] = 1, ['*/'] = -1, ['//'] = l.fold_line_comments('//')}
+}
+
+return M
