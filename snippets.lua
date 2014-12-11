df5c8d568f3996e760fd8323773bf97237ecb85a
Alejandro Baez <Alejan.Baez@gmail.com>
2014-12-11 16:19:21 -0500

    quick additions to snippets

--- snippets.lua
+++ snippets.lua
@@ -18,35 +18,41 @@ return {
   o     = "proc() {\n\t%0\n}", -- owned closure
 
 
-  -- Items and Attributes
+  -- Data Structures
   extern  = "extern %1(\"ABI\") {\n\t%0\n}",
   struct  = "struct %1(name) {\n\t%0\n}",
   enum    = "enum %1(name) {\n\t%0\n}",
   mod     = "mod %1(name) {\n\t%0\n}",
   trait   = "trait %1(name) {\n\t%0\n}",
-  impl    = "impl %1(name) %2(for %3(type)){\n\t%0\n}",
+  impl    = "impl %1(name) %2(for %3(type) ){\n\t%0\n}",
 
   -- Expressions
   v           = "[%1(start), %2(.. %3(range_end))]%0",
   ["while"]   = "while %1(expr) {\n\t%0\n}",
   loop        = "loop {\n\t%0\n}",
   ["for"]     = "for %1(i) in %2(iterator) {\n\t%0\n}",
-  ["forr"]     = "for %1(i) in range(%2(0), %3(10)) {\n\t%0\n}",
+  ["forr"]    = "for %1(i) in range(%2(0), %3(10)) {\n\t%0\n}",
 
   -- if
   ["if"]      = "if %1(expr) {\n\t%2\n} %0",
+  ifl         = "if let %1(pattern) = %2(expr) {\n\t%3\n} %0",
   ["else"]    = 'else {\n\t%0\n}',
   match       = "match %1(expr) {\n\t%2(pattern) => %3(expr)\n}",
 
-  -- random
-  ["#"]   = "#[%1(attribute)]",
-  static  = "static %1(name): %2(type) = %0;",
-  lmut    = "let mut %1(name): %2(type) = %0;",
+  -- let
   let     = "let %1(name): %2(type) = %0;",
-  ["/*"]  = "/*\n\t%0\n*/",
-  ["print"] = 'println!("{:%1(?)}\\n", %0);',
+  lmut    = "let mut %1(name): %2(type) = %0;",
+  lvec    = "let %1(mut) %2(name): Vec<%3(T)> = Vec::new();%0",
+  lbox    = "let %1(mut) %2(name): Box<%3(T)> = box %4(value);%0",
+
+  -- random
+  ["#"]     = "#%1(!)[%2(attribute)]%0",
+  crate     = "extern crate %1(name);%0",
+  static    = "static %1(name): %2(type) = %0;",
+  ["/*"]    = "/*\n\t%0\n*/",
+  ["print"] = 'println!("{%1(name)}\\n", %1);%0',
 
   -- tasks and communication
-  ["spawn"] = "spawn(proc() {\n\t%0\n});",
+  ["spawn"]   = "spawn(proc() {\n\t%0\n});",
   ["channel"] = "let (%1(tx, rx)): (Sender<%2(int)>, Receiver<%3(int)) = channel();%0",
 }
