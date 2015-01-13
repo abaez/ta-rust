c25d113b1b6537f8925074084dd0fea6e08ede5e
Alejandro Baez <Alejan.Baez@gmail.com>
2015-01-13 14:02:51 -0500

    fixed the tag build

--- tag_build.lua
+++ tag_build.lua
@@ -2,14 +2,17 @@ local parser = require("modules.rust.taparser")
 
 local tags_dir = _USERHOME .. "tags"
 
-function get_project_name()
+local function get_project_name()
   return (io.get_project_root()):gsub("%/.+%/", "")
 end
 
-function build(project)
+local function build(project)
   os.execute(string.format("ctags %s -R --rust-kinds=-c-d-T %s -f %s",
-    parser.parse_ctags(
-      io.open()),
-      tags_dir .. "/" .. get_project_name(),
-      get_project_name()))
+    parser.parse_ctags(_USERHOME .. "/modules/rust/ctags.rust"),
+    tags_dir .. "/" .. project,
+    project))
 end
+
+return {
+  build = build
+}
