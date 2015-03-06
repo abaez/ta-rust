03d9bcf7c67a601f95b062190f289bbd34493435
Alejandro Baez <Alejan.Baez@gmail.com>
2015-03-06 17:57:11 -0500

    lexer: change module call to local

--- rust.lua
+++ rust.lua
@@ -5,8 +5,8 @@
 -- @license MIT (see LICENSE)
 -- @module rust
 
-
-local l, token, word_match = lexer, lexer.token, lexer.word_match
+local l = require("lexer")
+local token, word_match = l.token, l.word_match
 local P, R, S = lpeg.P, lpeg.R, lpeg.S
 
 local M = {_NAME = 'rust'}
