e22f3cd0a8a0b229cda64456b061e2168c96a61f
Alejandro Baez <code@baez.nyc>
2015-09-08 21:45:19 -0400

    modulized keys

--- init.lua
+++ init.lua
@@ -12,7 +12,6 @@ local api = require("rust.builder.api")
 local tag = require("rust.builder.tag")
 local raw = require("rust.builder.raw")
 
-
 local function add_apitag(name, path)
   local user_api = path .. "/.api_" .. name
   local user_tag = path .. "/.tag_" .. name
@@ -47,41 +46,7 @@ if type(snippets) == 'table' then
 end
 
 --- Table of Rust-specific key bindings.
-keys.rust = {
-  [not OSX and not CURSES and 'cl' or 'ml'] = {
-    -- Open this module for editing: `Alt/⌘-L` `M`
-    s = { io.open_file,
-        (_USERHOME..'/modules/rust/snippets.lua') },
-  },
-
-  ['s\n'] = function()
-    buffer:line_end()
-    buffer:add_text(';')
-    buffer:new_line()
-  end,
-
-  ['a\n'] = function()
-    buffer:line_end()
-    buffer:new_line()
-    buffer:add_text("/// ");
-  end,
-
-  ['cB'] = function()
-    local project_name, project_path = raw.get_project_name()
-
-    if project_path then
-      local tmp = raw.build(project_path)
-
-      api.build(project_name, project_path, tmp)
-      tag.build(project_name, project_path, tmp)
-      os.remove(tmp)
-
-      add_apitag(project_name, project_path)
-    end
-
-    return textadept.run.build()
-  end,
-}
+keys.rust = require("modules.rust.keys")
 
 events.connect(events.LEXER_LOADED, function (lang)
   if lang ~= 'rust' then return end
