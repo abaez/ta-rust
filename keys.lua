91848f3657f0da7098d499bacb3d26caf91bdee7
Alejandro Baez <code@baez.nyc>
2016-09-30 18:11:37 -0400

    keys: small cleanup to formatting

--- keys.lua
+++ keys.lua
@@ -3,42 +3,50 @@ local tag = require("rust.builder.tag")
 local raw = require("rust.builder.raw")
 
 return {
-   [not OSX and not CURSES and 'cl' or 'ml'] = {
-    -- Open this module for editing: `Alt/⌘-L` `M`
-    s =
-      function() io.open_file(_USERHOME..'/modules/rust/snippets.lua') end
-  },
+  [not OSX and not CURSES and 'cl' or 'ml'] =
+    {
+      -- Open this module for editing: `Alt/⌘-L` `M`
+      s =
+        function() io.open_file(_USERHOME..'/modules/rust/snippets.lua') end
+    }
+  ,
 
-  ['s\n'] = function()
-    buffer:line_end()
-    buffer:add_text(';')
-    buffer:new_line()
-  end,
-
-  ['a\n'] = function()
-    buffer:line_end()
-    buffer:new_line()
-    buffer:add_text("/// ");
-  end,
-
-  ['cB'] = function()
-    local project_name, project_path = raw.get_project_name()
-
-    if project_path then
-      local tmp = raw.build(project_path)
-
-      api.build(project_name, project_path, tmp)
-      tag.build(project_name, project_path, tmp)
-      os.remove(tmp)
-
-      api.add_apitag(project_name, project_path)
+  ['s\n'] =
+    function()
+      buffer:line_end()
+      buffer:add_text(';')
+      buffer:new_line()
     end
+  ,
 
-    if io.open('../Cargo.toml') ~= nil then
-      textadept.run.compile()
-      return
+  ['a\n'] =
+    function()
+      buffer:line_end()
+      buffer:new_line()
+      buffer:add_text("/// ");
     end
+  ,
 
-    return textadept.run.build()
-  end,
+  ['cB'] =
+    function()
+      local project_name, project_path = raw.get_project_name()
+
+      if project_path then
+        local tmp = raw.build(project_path)
+
+        api.build(project_name, project_path, tmp)
+        tag.build(project_name, project_path, tmp)
+        os.remove(tmp)
+
+        api.add_apitag(project_name, project_path)
+      end
+
+      if io.open('../Cargo.toml') ~= nil then
+        textadept.run.compile()
+        return
+      end
+
+      return textadept.run.build()
+    end
+  ,
 }
